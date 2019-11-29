913890b45e016a7411febd292ba3c28d29da677f
chenxiaofen <852076508@qq.com>
2019-11-29 17:59:20 +0800

    add localStorage

--- src/app2.js
+++ src/app2.js
@@ -3,12 +3,15 @@ import $ from "jquery";
 
 const $tabBar = $("#app2 .tab-bar");
 const $tabContent = $("#app2 .tab-content");
+const localKey = 'app2.active'
+const index = localStorage.getItem(localKey) || 0
 
 $tabBar.on('click', 'li', (e) => {
   let $li = $(e.currentTarget)
   const index = $li.index()
+  localStorage.setItem(localKey, index)
   $li.addClass('selected').siblings().removeClass('selected')
   $tabContent.children().eq(index).addClass('active').siblings().removeClass('active')
 })
 // 默认点击第一个
-$tabBar.children().eq(0).trigger('click')
+$tabBar.children().eq(index).trigger('click')
